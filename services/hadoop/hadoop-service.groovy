69d9314b6cf83e747f09f076f04cd973f10e0bc6
elaadt <elaadt@gigaspaces.com>
2012-07-30 15:35:40 +0300

    Update services/hadoop/hadoop-service.groovy
    Added monitoring UI defs for NameNode stats.

--- services/hadoop/hadoop-service.groovy
+++ services/hadoop/hadoop-service.groovy
@@ -48,9 +48,104 @@ service {
 
 	}
 	
+	userInterface {
 
+		metricGroups = ([
+			metricGroup {
 
+				name "HadoopNameNodeStats"
 
+				metrics([
+					"Number of active metrics sources",
+					"Number of active metrics sinks",
+					"Number of ops for snapshot stats",
+					"Average time for snapshot stats",
+					"Number of ops for publishing stats",
+					"Average time for publishing stats",
+					"Dropped updates by all sinks",
+				])
+			} ,
 
-	
+		]
+		)
+
+		widgetGroups = ([
+			widgetGroup {
+				name "Number of active metrics sources"
+				widgets ([
+					balanceGauge{metric = "Number of active metrics sources"},
+					barLineChart{
+						metric "Number of active metrics sources"
+						axisYUnit Unit.REGULAR
+					}
+				])
+			},
+			widgetGroup {
+				name "Number of active metrics sinks"
+				widgets([
+					balanceGauge{metric = "Number of active metrics sinks"},
+					barLineChart {
+						metric "Number of active metrics sinks"
+						axisYUnit Unit.REGULAR
+					}
+				])
+			},
+			widgetGroup {
+				name "Number of ops for snapshot stats"
+				widgets ([
+					balanceGauge{metric = "Number of ops for snapshot stats"},
+					barLineChart{
+						metric "Number of ops for snapshot stats"
+						axisYUnit Unit.REGULAR
+					}
+				])
+			},
+			widgetGroup {
+
+				name "Average time for snapshot stats"
+				widgets([
+					balanceGauge{metric = "Average time for snapshot stats"},
+					barLineChart {
+						metric "Average time for snapshot stats"
+						axisYUnit Unit.REGULAR
+					}
+				])
+			} ,
+			widgetGroup {
+
+				name "Number of ops for publishing stats"
+				widgets([
+					balanceGauge{metric = "Number of ops for publishing stats"},
+					barLineChart {
+						metric "Number of ops for publishing stats"
+						axisYUnit Unit.REGULAR
+					}
+				])
+			} ,
+			widgetGroup {
+
+				name "Average time for publishing stats"
+				widgets([
+					balanceGauge{metric = "Average time for publishing stats"},
+					barLineChart {
+						metric "Average time for publishing stats"
+						axisYUnit Unit.REGULAR
+					}
+				])
+			} ,
+			widgetGroup {
+
+				name "Dropped updates by all sinks"
+				widgets([
+					balanceGauge{metric = "Dropped updates by all sinks"},
+					barLineChart {
+						metric "Dropped updates by all sinks"
+						axisYUnit Unit.REGULAR
+					}
+				])
+			} ,
+		]
+		)
+	}
+
 }
