0ec009746ce47741eb87f2578594317792efc02b
Itai Frenkel <itaifrenkel@live.com>
2012-07-30 20:58:01 +0300

    Code Cleanup

--- services/hadoop/Hadoop.groovy
+++ services/hadoop/Hadoop.groovy
@@ -4,30 +4,9 @@ import org.cloudifysource.dsl.context.ServiceContext;
 
 class Hadoop {
 
-def static workingDir
-def static nameServicePort = "50070"
-def static ip = ServiceUtils.getPrimaryInetAddress(); 
-//def static context;
-//def static config;
-//def static installDir;
-
-def static init(serviceContext) {
- 
-  //context = serviceContext
-  println "Aftyer Context"
-  //config = new ConfigSlurper().parse(new File("${context.serviceDirectory}/hadoop-service.properties").toURL())
-  println "Aftyer Config"
-
-  //installDir = "${System.properties["user.home"]}/.cloudify/${context.applicationName}_${context.serviceName}_${context.instanceId}"
-  println "Aftyer installDir" 
-  //workingDir = context.serviceDirectory
-  nameServicePort = "50070"
-    
-}
-
 def static boolean isNameNodeRuning(){
-  def output =serviceCmd("hadoop-hdfs-namenode status");
-  return (output.contains("[OK]"));
+  def ret = serviceCmd("hadoop-hdfs-namenode status");
+  return ret.exitcode == 0
 }
 
 def static void install() {
@@ -35,62 +14,66 @@ def static void install() {
 }
 
 static void start () {
-  if (isNameNodeRuning())
-   stop();
   startNameNode();
-  startDataNode();
-  startSecondaryNode();
-  
+  //startDataNode();
+  //startSecondaryNode();  
 }
 
 static void stop() {
-  stopDataNode();
-  stoptSecondaryNode();
+  //stopDataNode();
+  //stoptSecondaryNode();
   stopNameNode();
 }
 
 def static  startDataNode(){
-  serviceCmd("hadoop-hdfs-datanode start"); 
+  serviceCmdThrowOnExitCode("hadoop-hdfs-datanode start"); 
 }
 
 def static startNameNode(){
-  serviceCmd("hadoop-hdfs-namenode start ${ip}:${nameServicePort}")
+  def nameServicePort = "50070"
+  def ipAddress = ServiceUtils.getPrimaryInetAddress();
+  serviceCmdThrowOnExitCode("hadoop-hdfs-namenode start ${ipAddress}:${nameServicePort}")
    
 }
 
 def static startSecondaryNode(){
-  serviceCmd("hadoop-hdfs-secondarynamenode start");
+  serviceCmdThrowOnExitCode("hadoop-hdfs-secondarynamenode start");
 }
 
 def static stopDataNode(){
- serviceCmd("hadoop-hdfs-datanode stop"); 
+ serviceCmdThrowOnExitCode("hadoop-hdfs-datanode stop"); 
 }
 
 def static stopNameNode(){
- serviceCmd("hadoop-hdfs-namenode stop");
+ serviceCmdThrowOnExitCode("hadoop-hdfs-namenode stop");
 }
 
 def static stopSecondaryNode(){
-  serviceCmd("hadoop-hdfs-secondarynamenode stop");
+  serviceCmdThrowOnExitCode("hadoop-hdfs-secondarynamenode stop");
 }
 
 
-static String serviceCmd(args)  {
+/**
+ * Returns an object with the following properties
+ * stdout, stderr, and exitcode
+ */
+static def serviceCmd(args)  {
       def ant = new AntBuilder()
-      ant.exec(outputproperty:"cmdOut", //includes stdout and stderr
-             resultproperty:"cmdExit",
-             failonerror: "false",
+      ant.exec(outputproperty:"stdout", 
+ 		   errorproperty:"stderr"
+             resultproperty:"exitcode",
              executable: "service") {
                 arg(line:args)
              }
-      String cmdOut = ant.project.properties.cmdOut
-      Integer cmdExit = ant.project.properties.cmdExit 
-      /* Rem'd by Elaad because cmdExit != 0 does not imply failure
-      if (cmdExit != 0 && !args.contains("status")) {
-        throw new Exception("command \"service ${args}\" exit code ${cmdExit} output: ${cmdOut}")
-      }
-      */
-      return cmdOut
+      return ant.project.properties;
+}
+
+static def serviceCmdThrowOnExitCode(args)  {
+	def ret = serviceCmd(args)
+	  if (ret.exitcode != 0) {
+         throw new Exception("command \"service ${args}\" exit code ${ret.exitcode} stdout: ${ret.stdout} stderr: ${ret.stderr}")
+       }
+	return ret;
 }
 
 }
