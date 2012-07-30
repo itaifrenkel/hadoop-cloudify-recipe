16c9093e57b70897076d71df4cbc1ab6bedd05f8
Itai Frenkel <itaif@gigaspaces.com>
2012-07-30 22:38:37 +0300

    troubleshooting jmx issues

--- services/hadoop/hadoop-service.groovy
+++ services/hadoop/hadoop-service.groovy
@@ -54,7 +54,7 @@ service {
 				"Dropped updates by all sinks": ["Hadoop:name=MetricsSystem,service=NameNode,sub=Stats", "DroppedPubAll"],
 			]
 			
-			return getJmxMetrics("127.0.0.1",11099,nameNodeJmxBeans)
+			return getJmxMetrics("127.0.0.1",nameNodeJmxPort,nameNodeJmxBeans)
 			//return getJmxMetrics(ServiceUtils.getPrimaryInetAddress(),nameNodeJmxPort,nameNodeJmxBeans)
         }
 
