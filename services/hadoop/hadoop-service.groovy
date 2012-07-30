554a8e3179611ae8c4f8f89c4d7ec2bb2a36b54f
esteratz <ester@gigaspaces.com>
2012-07-30 11:22:55 +0300

    Update services/hadoop/hadoop-service.groovy

--- services/hadoop/hadoop-service.groovy
+++ services/hadoop/hadoop-service.groovy
@@ -9,22 +9,28 @@ service {
 	numInstances 1
 	minAllowedInstances 1
 	maxAllowedInstances 2
+	hadoopx = null;
+	def hadoop = {
+		if (hadoopx == null)
+			hadoopx =new Hadoop()
 	
-	hadoop = new Hadoop();
+		return hadoopx
+		
+	}
 	
 	compute {
 		template "SMALL_LINUX"
 	}
 
 	lifecycle {
-		install { hadoop.install() }
-		start { hadoop.startNameNode() }
+		install { hadoop().install() }
+		start { hadoop().startNameNode() }
 	}
 	
 
 	startDetection {
     	//	ServiceUtils.isPortsOccupied(hadoop.nameServicePort, "127.0.0.1") &&
-    		hadoop.isNameServiceRuning()
+    		hadoop().isNameServiceRuning()
 	}
 
 
