d23fe8b29f9c5708fd821dfa28ab0bc53fa1838d
esteratz <ester@gigaspaces.com>
2012-07-31 10:28:55 +0300

    Update services/hadoop/LinuxService.groovy

--- services/hadoop/LinuxService.groovy
+++ services/hadoop/LinuxService.groovy
@@ -41,6 +41,28 @@ static def serviceCmd(args)  {
       return ret;
 }
 
+static def hdfsCmd(args)  {
+      def ant = new AntBuilder()
+      ant.exec(outputproperty:"stdout", 
+ 		       errorproperty:"stderr",
+             resultproperty:"exitcode",
+             executable: "hdfs") {
+                arg(line:args)
+             }
+	  def ret = ant.project.properties;
+	  ret.exitcode = ret.exitcode as int;
+      return ret;
+}
+
+static def hdfsCmdThrowOnExitCode(args)  {
+	def ret = hdfsCmd(args)
+	  if (ret.exitcode != 0) {
+         throw new Exception("command \"hdfs ${args}\" exit code ${ret.exitcode} stdout: ${ret.stdout} stderr: ${ret.stderr}")
+       }
+	return ret;
+}
+
+
 /**
  * Returns an object with the following properties
  * stdout, stderr
